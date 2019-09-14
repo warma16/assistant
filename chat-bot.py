5f0d7340d80d119dba83053d3b3de7b2f0d81c1c
yueiuieui <13163421396@163.com>
2019-09-14 21:32:12 +0800

    更新了健康管理的睡眠

--- chat-bot.py
+++ chat-bot.py
@@ -1,6 +1,6 @@
 
 from threading import Thread
-
+import datetime
 from confim.send import *
 nlp_token=get_ac('4cbT6K6fvhDkcfVayvnXdB8K','hGCUvZbGKakCGpTYDyv9PGWNrSvyR7ic')
 import time
@@ -13,11 +13,16 @@ pl=pathy.split('/')
 del pl[0]
 del pl[-2:]
 
-if sc == 'Linux':
-    pl[0]='/'+pl[0]
-    path='/'.join(pl)
-def health(x):
+def healtht(x):
     while 1:
+        if datetime.datetime.now().hour == 12:
+            make_voice('该吃饭了，同志。',show=True) 
+        if datetime.datetime.now().hour == 9+12:
+            make_voice('该睡觉了，同志。',show=True)
+        if datetime.datetime.now().hour == 9+12+8-24:
+            make_voice('该起床了，同志。',show=True)
+            playsound(os.getcwd()+'/ring/ring.mp3') 
+def healthr(x):   
         for i in range(0,25):
             time.sleep(60)
         make_voice('该休息了，同志。',show=True)
@@ -28,8 +33,11 @@ def health(x):
         playsound(os.getcwd()+'/ring/ring.mp3')
         make_voice('敲击一下回车键就我可以正常工作。',show=True)
 make_voice('主人您好，我是您的助理小程。输入文字可以跟我聊天呦。',show=True)
-p1=Thread(target=health,args=('h',))
-p1.setDaemon(True)  # thread1,它做为程序主线程的守护线程,当主线程退出时,thread1线程也会退出,由thread1启动的其它子线程会同时退出,不管是否执行完任务
+p1=Thread(target=healthr,args=('h',))
+p1.setDaemon(True) 
+p2=Thread(target=healtht,args=('h',))
+p2.setDaemon(True)
+p2.start()
 p1.start()
 while 1:
     ij=input(': ')
@@ -38,7 +46,6 @@ while 1:
         exit(0)
     elif '升级' in ij or 'update' in ij:
         os.system('python3 ./confim/update.py')
-        os.chdir(path+'/assistant')
         make_voice('小程已经更新完毕，请输入命令重新启动。',show=True)
         exit(0)
     else:
